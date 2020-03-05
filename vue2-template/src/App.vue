9f8c84f319dba74626c77ecfa28bcf66f98cede3
wtc <forever_lwy@163.com>
2020-03-05 18:07:14 +0800

    创建模板文件

--- vue2-template/src/App.vue
+++ vue2-template/src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="app">
+  <%_ if (modules.includes('router')) { -%>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  <%_ } else { -%>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <%_ } -%>
+    
   </div>
 </template>
 
@@ -16,7 +25,11 @@ export default {
 }
 </script>
 
+<%_ if (modules.includes('sass')) { -%>
+<style lang="scss">
+<%_ } else { -%>
 <style>
+<%_ } -%>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +38,34 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+<%_ if (modules.includes('router')) { -%>
+  <%_ if (modules.includes('sass')) { -%>
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+  <%_ } else { -%>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+  <%_ } -%>
+<%_ } -%>
 </style>
