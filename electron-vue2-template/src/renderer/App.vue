1f4523327d96ad861ea64380ec686d4ab671add5
wtc <forever_lwy@163.com>
2020-03-10 15:56:11 +0800

    init

---
+++ electron-vue2-template/src/renderer/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <landing-page></landing-page>
+  </div>
+</template>
+
+<script>
+  import LandingPage from '@/components/LandingPage'
+
+  export default {
+    name: 'electron-vue2-template',
+    components: {
+      LandingPage
+    }
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
